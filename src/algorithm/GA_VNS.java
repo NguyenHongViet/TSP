fd58cb00e4d33209ba894b3e5c9f6c45b85a17ce
Nguyen Hong Viet <banviet2222@gmail.com>
2015-03-18 20:48:09 +0700

    Fix GA_VNS

--- src/algorithm/GA_VNS.java
+++ src/algorithm/GA_VNS.java
@@ -1,12 +1,42 @@
 package algorithm;
 
+import java.util.ArrayList;
+
+import model.Solution;
+
 
 public class GA_VNS extends GA {
 	
-	GVNS gvns;
+	VNS vns;
 	
 	public GA_VNS(String filename) {
 		super(filename);
+		vns = new VNS(filename);
+	}
+	
+	@Override
+	public ArrayList<Solution> selection(ArrayList<Solution> population) {
+		Solution[] elite = new Solution[10];
+		elite[0] = population.get(0);
+		for (int i=1; i<elite.length; i++)
+			elite[i] = null;
+		
+		for (int i=1; i<population.size(); i++) {
+			double cost = population.get(i).getCost();
+			for (int j=0; (j < elite.length) || (elite[j] == null); j++) {
+				if (cost < elite[j].getCost()) {
+					for (int k=elite.length-1; k>j; k--)
+						elite[k] = elite[k-1];
+					elite[j] = population.get(i);
+					break;
+				}
+			}
+		}
+		
+		for (int i=0; i<elite.length; i++)
+			population.add(vns.algorithm(elite[i], 3, 5));
+		
+		return super.selection(population);
 	}
 	
 	public static void main(String[] args) {
